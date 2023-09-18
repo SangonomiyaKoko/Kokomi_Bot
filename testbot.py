a9a10f0af7e7f5dbc3de6a3ab8cce2a901c28c60
SangonomiyaKoko <103585196+SangonomiyaKoko@users.noreply.github.com>
2023-09-18 21:33:28 +0800

    Update testbot.py

--- testbot.py
+++ testbot.py
@@ -1,9 +1,47 @@
 import asyncio
 import traceback
-#from kokomi_bot.command_select import select_funtion
-from nonebot_plugin_kokomi.command_select import select_funtion
+from kokomi_bot.command_select import select_funtion
+from kokomi_bot.scripts.config import BLACKLIST, WHITELIST, FUNCTION_CONFIG
+
+import subprocess
+
+try:
+    import PIL
+except ImportError:
+    try:
+        subprocess.check_call(["pip", "install", 'pillow' + "==" + '9.5.0'])
+        print("{}库 安装成功！".format('pillow'))
+    except subprocess.CalledProcessError as e:
+        print("{}库 安装失败！错误信息：{}".format('pillow', e))
+else:
+    print("Pillow库 已存在,版本：", PIL.__version__)
+
+try:
+    import httpx
+except ImportError:
+    try:
+        subprocess.check_call(["pip", "install", 'httpx'])
+        print("{}库 安装成功！".format('httpx'))
+    except subprocess.CalledProcessError as e:
+        print("{}库 安装失败！错误信息：{}".format('httpx', e))
+else:
+    print("httpx库 已存在,版本：", httpx.__version__)
+
+try:
+    import cv2
+except ImportError:
+    try:
+        subprocess.check_call(["pip", "install", 'opencv-python'])
+        print("{}库 安装成功！".format('cv2'))
+    except subprocess.CalledProcessError as e:
+        print("{}库 安装失败！错误信息：{}".format('cv2', e))
+else:
+    print("cv2库 已存在")
+
+
+#from nonebot_plugin_kokomi.command_select import select_funtion
 user_id = '3197206779'
-message = 'wws me'
+message = 'wws me info'
 user_list = {
     '3197206779': 'test_account_0',
     '1835185912': 'test_account_1',
@@ -28,6 +66,12 @@ async def main():
         gruop_id = '677135822'
         group_name = 'None'
         group_data = None
+        # 黑名单
+        if gruop_id in BLACKLIST:
+            return
+        # 白名单
+        if gruop_id != None and WHITELIST != [] and gruop_id not in WHITELIST:
+            return
         if gruop_id != None and 'group' in split_msg:
             group_data = user_list
             group_info = [{'group_id': 164933984,
@@ -44,11 +88,16 @@ async def main():
             group_name=group_name,
             group_data=group_data
         )
+        print(fun)
         if fun['status'] == 'default':
             print('DEFAULT:', '无对应功能')
         elif fun['status'] == 'info':
             print('INFO:', fun['message'])
         else:
+            # 判断是否被关闭
+            if FUNCTION_CONFIG[fun['index']][0] != True:
+                print('该功能已关闭')
+                return
             function = fun['function']
             result = await function(fun['parameter'])
             if result['status'] == 'ok':
